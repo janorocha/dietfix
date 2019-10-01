4fa79c680cdb155e9e1005c03caf4c211b9c821b
Jano <rocha.jano@gmail.com>
2019-10-01 18:29:54 -0300

    frontend

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início" 
+        subtitle="Nutricionista e Endocrinologista" >
+        <div className="display-4">Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Cadastro de pacientes.</p>
+    </ Main>
---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Copyright <i className="fa fa-copyright"></i> 2019 <strong className="text-danger">PJ</strong> Ltda
+        </span>
+    </footer>
