3744f2e5cb1af929194aca03fb7c6ae926224717
Jano <rocha.jano@gmail.com>
2019-10-10 22:01:10 -0300

    iniciando a agenda

--- frontend/src/main/Routes.jsx
+++ frontend/src/main/Routes.jsx
@@ -3,10 +3,12 @@ import { Switch, Route, Redirect } from 'react-router'
 
 import Home from '../components/home/Home'
 import UserCrud from '../components/user/UserCrud'
+import Agenda from '../components/agenda/agenda'
 
 export default props =>
     <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/pacientes' component={UserCrud} />
+        <Route path='/agenda' component={Agenda} />
         <Redirect from='*' to= '/' />
     </Switch>
